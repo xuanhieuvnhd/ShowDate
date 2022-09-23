c2694e2988c6d3387bd7fd2e4e95371225a4591d
xuanhieuvnhd <xuanhieuvn@gmail.com>
2022-09-23 10:01:09 +0700

    23/09

---
+++ src/main/java/com/example/showdate/Demo.java
@@ -0,0 +1,22 @@
+package com.example.showdate;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+@WebServlet(name = "Demo",value = "/demo")
+public class Demo extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html>");
+        Date today = new Date();
+        writer.println("<h1>" + today + "</h1>");
+        writer.println("</html>");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Thời gian hiện tại của hệ thống</title>
+</head>
+<body>
+<h1><%= "Chúc mừng năm mới" %>
+</h1>
+<br/>
+<a href="/demo"><center>ShowDate</center></a>
+</body>
+</html>
